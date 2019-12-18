1411b9b9fa046a1bbe2cb5162794674648bf9d38
Nishkal <nishkal8463@gmail.com>
2019-12-18 12:21:32 +0530

    Updates

---
+++ precache-manifest.c2c2b048bfb62e606318a4d0aff02f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1c755fbaa8e87836604a3aa35f969e",
+    "url": "/MovieFlix/index.html"
+  },
+  {
+    "revision": "a9ea7a3b9d414e78906a",
+    "url": "/MovieFlix/static/css/main.cee89b7a.chunk.css"
+  },
+  {
+    "revision": "e6ebb8ad65aa6fb9b3ef",
+    "url": "/MovieFlix/static/js/2.ef8563d2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MovieFlix/static/js/2.ef8563d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9ea7a3b9d414e78906a",
+    "url": "/MovieFlix/static/js/main.f5ab267d.chunk.js"
+  },
+  {
+    "revision": "16290cc29b02ea0d1804",
+    "url": "/MovieFlix/static/js/runtime-main.b83324fe.js"
+  }
+]);
